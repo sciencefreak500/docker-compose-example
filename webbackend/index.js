a87065cc2d9e06e3dc9fb9cde53e42ce3d689e85
Milord <immunitygame@gmail.com>
2019-08-06 13:33:20 -0400

    cors

--- webbackend/index.js
+++ webbackend/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 3000
+
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/random', (req, res) => res.send(Math.floor(Math.random()*10)))
+app.get('/random', (req, res) => {
+	res.send("Random #" + Math.floor(Math.random()*10).toString())
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
